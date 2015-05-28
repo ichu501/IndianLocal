98f2e0af957168cb023b675bd4e5c328b37ce830
IRSHAD <ichu501@gmail.com>
2015-05-28 15:26:55 +0530

    Add Indian regional languges
    Adde indian longings like Bengali
    Gujarati
    Hindi
    Kannada
    Malayalam
    Marathi
    Oriya
    Tamil
    Telugu

---
+++ Language Changer/ViewController.m
@@ -0,0 +1,135 @@
+//
+//  ViewController.m
+//  Language Changer
+//
+//  Created by Alan Chung on 25/11/2014.
+//  Copyright (c) 2014 Alan Chung. All rights reserved.
+//
+
+#import "ViewController.h"
+#import "LanguageManager.h"
+#import "Locale.h"
+#import "Constants.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    
+    @try {
+        [super viewDidLoad];
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        self.flagImageView.contentMode = UIViewContentModeScaleAspectFit;
+        
+        LanguageManager *languageManager = [LanguageManager sharedLanguageManager];
+        
+        NSInteger selectedIndex = 0;
+        
+        Locale *selectedLocale = [languageManager getSelectedLocale];
+        
+        selectedIndex = [languageManager.availableLocales indexOfObject:selectedLocale];
+        
+        // Move the picker to match what was selected previously.
+        [self.localePicker selectRow:selectedIndex inComponent:0 animated:YES];
+        
+        [self setupLocalisableElements];
+    }
+    @catch (NSException *exception) {
+        NSLog(@"%@",exception);
+    }
+   
+    
+    
+   
+}
+
+- (void)viewDidAppear:(BOOL)animated {
+    
+    // Scroll back to the top of the text.
+    self.textView.contentOffset = CGPointZero;
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - Localisation
+
+/*!
+ * @function setupLocalisableElements
+ *
+ * @abstract
+ * Update the strings and images in the view depending on the currently selected localisation.
+ *
+ * @discussion
+ * Should be called whenever the localisation is set.
+ */
+- (void)setupLocalisableElements {
+    
+    
+    
+    // Flag images are named after the country code of the Localisation.
+//    UIImage *flagImage = [UIImage imageNamed:[[LanguageManager sharedLanguageManager] getSelectedLocale].countryCode];
+//    [self.flagImageView setImage:flagImage];
+    
+    NSString *lang = [[[LanguageManager sharedLanguageManager] getSelectedLocale] languageCode];
+   
+    self.title = CustomLocalisedString(@"Title", @"The string to display in the navigation bar.");
+    
+    self.textView.text = CustomLocalisedString(@"Text", @"The string to display in the text view.");
+    NSLog(@"%@",self.textView.text);
+    
+    if ([lang isEqualToString:@"ml"]) {
+//        self.textView.font = [UIFont fontWithName:@"MalayalamSangamMN" size:18];
+        [self.textView setFont:[UIFont fontWithName:@"MalayalamSangamMN" size:18]];
+    }
+    self.textView.contentOffset = CGPointZero;
+   
+    
+   
+}
+
+#pragma mark - UIPickerViewDataSource
+
+- (NSInteger)numberOfComponentsInPickerView:(UIPickerView *)pickerView {
+    
+    return 1;
+}
+
+- (NSInteger)pickerView:(UIPickerView *)pickerView numberOfRowsInComponent:(NSInteger)component {
+    
+    return [LanguageManager sharedLanguageManager].availableLocales.count;
+}
+
+#pragma mark - UIPickerViewDelegate
+
+- (NSString *)pickerView:(UIPickerView *)pickerView titleForRow:(NSInteger)row forComponent:(NSInteger)component {
+    
+    Locale *localeForRow = [LanguageManager sharedLanguageManager].availableLocales[row];
+    
+    return localeForRow.name;
+}
+
+- (void)pickerView:(UIPickerView *)pickerView didSelectRow:(NSInteger)row inComponent:(NSInteger)component {
+    
+    /*
+     * Set the localisation that the user has just picked.
+     */
+    
+    LanguageManager *languageManager = [LanguageManager sharedLanguageManager];
+    
+    Locale *localeForRow = languageManager.availableLocales[row];
+    
+    NSLog(@"Language selected: %@", localeForRow.name);
+    
+    [languageManager setLanguageWithLocale:localeForRow];
+    
+    [self setupLocalisableElements];
+}
+
+@end
---
+++ README.md
@@ -0,0 +1,8 @@
+ios-language-changer
+====================
+
+iOS language changer example app
+
+A simple app that allows the user to change language in-app, independent of the device setting.
+
+See http://createdineden.com/blog/2014/december/12/language-changer-in-app-language-selection-in-ios/ for the accompanying tutorial.
